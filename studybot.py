cf48e6af1db4365168666aee757fa45fd3e54aad
Baburaomasthai <anyusefor0@gmail.com>
2025-09-29 18:12:25 +0530

    Update studybot.py

--- studybot.py
+++ studybot.py
@@ -1,3 +1,20 @@
+from flask import Flask
+import threading
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "✅ 𝑇𝑟𝑦𝑖𝑛𝑔 𝑇𝑜 𝑇𝑎𝑐𝑘𝑙𝑒 𝑆𝑒𝑡𝑏𝑎𝑐𝑘 𝑇𝐺 - @MrJaggiX!"
+
+def run_flask():
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port)
+
+# Flask ko background thread me start karo
+threading.Thread(target=run_flask).start()
+
 import json
 import os
 from typing import Dict, Any, List
